863d9394e62ec3787acfcd89c262b11aa1e53caf
Shah Faisal <shahfaisalgfg@gmail.com>
2025-08-02 17:23:15 +0500

    initial commit

---
+++ ContactsManager/ContactsManager_v2.0.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ContactsManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnLoadContacts = new System.Windows.Forms.Button();
+            this.btnSaveAll = new System.Windows.Forms.Button();
+            this.btnCSV = new System.Windows.Forms.Button();
+            this.btnSaveAllExcel = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControlContacts = new System.Windows.Forms.TabControl();
+            this.tabControlContacts.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(24, 15);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(86, 16);
+            this.lblFilePath.TabIndex = 1;
+            this.lblFilePath.Text = "Selected File";
+            // 
+            // btnLoadContacts
+            // 
+            this.btnLoadContacts.Location = new System.Drawing.Point(541, 8);
+            this.btnLoadContacts.Name = "btnLoadContacts";
+            this.btnLoadContacts.Size = new System.Drawing.Size(161, 33);
+            this.btnLoadContacts.TabIndex = 2;
+            this.btnLoadContacts.Text = "Load Contacts File";
+            this.btnLoadContacts.UseVisualStyleBackColor = true;
+            this.btnLoadContacts.Click += new System.EventHandler(this.btnLoadContacts_Click);
+            // 
+            // btnSaveAll
+            // 
+            this.btnSaveAll.Location = new System.Drawing.Point(708, 322);
+            this.btnSaveAll.Name = "btnSaveAll";
+            this.btnSaveAll.Size = new System.Drawing.Size(84, 78);
+            this.btnSaveAll.TabIndex = 4;
+            this.btnSaveAll.Text = "Save All As Text";
+            this.btnSaveAll.UseVisualStyleBackColor = true;
+            this.btnSaveAll.Visible = false;
+            this.btnSaveAll.Click += new System.EventHandler(this.btnSaveAll_Click);
+            // 
+            // btnCSV
+            // 
+            this.btnCSV.Location = new System.Drawing.Point(708, 238);
+            this.btnCSV.Name = "btnCSV";
+            this.btnCSV.Size = new System.Drawing.Size(84, 78);
+            this.btnCSV.TabIndex = 5;
+            this.btnCSV.Text = "Save As Importable CSV";
+            this.btnCSV.UseVisualStyleBackColor = true;
+            this.btnCSV.Visible = false;
+            this.btnCSV.Click += new System.EventHandler(this.btnCSV_Click);
+            // 
+            // btnSaveAllExcel
+            // 
+            this.btnSaveAllExcel.Location = new System.Drawing.Point(708, 154);
+            this.btnSaveAllExcel.Name = "btnSaveAllExcel";
+            this.btnSaveAllExcel.Size = new System.Drawing.Size(84, 78);
+            this.btnSaveAllExcel.TabIndex = 6;
+            this.btnSaveAllExcel.Text = "Save All As Excel";
+            this.btnSaveAllExcel.UseVisualStyleBackColor = true;
+            this.btnSaveAllExcel.Visible = false;
+            this.btnSaveAllExcel.Click += new System.EventHandler(this.btnSaveAllExcel_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(667, 337);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(667, 337);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabControlContacts
+            // 
+            this.tabControlContacts.Controls.Add(this.tabPage1);
+            this.tabControlContacts.Controls.Add(this.tabPage2);
+            this.tabControlContacts.Location = new System.Drawing.Point(27, 47);
+            this.tabControlContacts.Name = "tabControlContacts";
+            this.tabControlContacts.SelectedIndex = 0;
+            this.tabControlContacts.Size = new System.Drawing.Size(675, 366);
+            this.tabControlContacts.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveAllExcel);
+            this.Controls.Add(this.btnCSV);
+            this.Controls.Add(this.btnSaveAll);
+            this.Controls.Add(this.tabControlContacts);
+            this.Controls.Add(this.btnLoadContacts);
+            this.Controls.Add(this.lblFilePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ContactsManager_v2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControlContacts.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Button btnLoadContacts;
+        private System.Windows.Forms.Button btnSaveAll;
+        private System.Windows.Forms.Button btnCSV;
+        private System.Windows.Forms.Button btnSaveAllExcel;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControlContacts;
+    }
+}
+
---
+++ ContactsManager/ContactsManager_v2.0.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace ContactsManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabControlContacts.TabPages.Clear();
+        }
+
+
+        private string GetCountryCode(string phoneNumber)
+        {
+            // Assuming country code is at the beginning of the phone number, preceded by a '+'
+            if (phoneNumber.StartsWith("+"))
+            {
+                //Note: If you add more country codes then uncomment "retun countryCode" and remove line "return Pakistani"
+                string[] validCountryCodes = {
+            "+92","+91"
+        };
+
+                // Iterate through the valid country codes in descending order of length
+                foreach (string countryCode in validCountryCodes.OrderByDescending(code => code.Length))
+                {
+                    if (phoneNumber.StartsWith("+92"))
+                    {
+                        //If you add more country codes then uncomment "retun countryCode" and remove line "return Pakistani"
+                        //return countryCode;
+                        return "P";
+
+                    }
+                    if(phoneNumber.StartsWith("+91"))
+                    {
+                        return "I";
+                    }
+                }
+                return "O";
+            }
+
+            // Default to an empty string if no valid country code is found
+            return string.Empty;
+        }
+
+        private void btnLoadContacts_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "Text Files (*.txt)|*.txt|Excel Files (*.xlsx)|*.xlsx";
+            openFileDialog.Filter = "Custom Files (*.txt, *.xlsx)|*.txt;*.xlsx";
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                lblFilePath.Text = filePath;
+                btnSaveAll.Visible = true;
+                btnCSV.Visible = true;
+                btnSaveAllExcel.Visible = true;
+
+                if (filePath.EndsWith(".txt"))
+                {
+                    LoadContactsFromTextFile(filePath);
+                }
+                else if (filePath.EndsWith(".xlsx"))
+                {
+                    LoadContactsFromExcelFile(filePath);
+                }
+                else
+                {
+                    MessageBox.Show("Unsupported file format. Please select a .txt or .xlsx file.");
+                }
+            }
+        }
+
+        private void LoadContactsFromTextFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            tabControlContacts.TabPages.Clear();
+
+            HashSet<string> contactsSet = new HashSet<string>();
+            foreach (string contact in lines)
+            {
+                string cleanedContact = CleanContact(contact);
+                if (!string.IsNullOrEmpty(cleanedContact))
+                {
+                    contactsSet.Add(cleanedContact);
+                }
+            }
+
+            foreach (string contact in contactsSet)
+            {
+                string countryCode = GetCountryCode(contact);
+                TabPage tabPage = FindOrCreateTabPage(countryCode);
+                ListBox listBox = GetListBoxForTabPage(tabPage);
+                listBox.Items.Add(contact);
+            }
+
+            MessageBox.Show("Contacts loaded successfully");
+        }
+
+        private void LoadContactsFromExcelFile(string filePath)
+        {
+            tabControlContacts.TabPages.Clear();
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                foreach (var worksheet in package.Workbook.Worksheets)
+                {
+                    int startRow = worksheet.Dimension.Start.Row;
+                    int endRow = worksheet.Dimension.End.Row;
+
+                    HashSet<string> contactsSet = new HashSet<string>();
+
+                    for (int row = startRow; row <= endRow; row++)
+                    {
+                        var cell = worksheet.Cells[row, 1];
+                        string contact = cell.Text.Trim();
+
+                        if (!string.IsNullOrEmpty(contact))
+                        {
+                            string cleanedContact = CleanContact(contact);
+                            if (!string.IsNullOrEmpty(cleanedContact))
+                            {
+                                contactsSet.Add(cleanedContact);
+                            }
+                        }
+                    }
+
+                    foreach (string cleanedContact in contactsSet)
+                    {
+                        string countryCode = GetCountryCode(cleanedContact);
+                        TabPage tabPage = FindOrCreateTabPage(countryCode);
+                        ListBox listBox = GetListBoxForTabPage(tabPage);
+                        listBox.Items.Add(cleanedContact);
+                    }
+                }
+            }
+
+            MessageBox.Show("Contacts loaded successfully.");
+        }
+
+
+        private string CleanContact(string contact)
+        {
+            // Keep only the valid characters: "+", "0"-"9"
+            string validCharacters = "+0123456789";
+            return new string(contact.Where(c => validCharacters.Contains(c)).ToArray());
+        }
+
+        private TabPage FindOrCreateTabPage(string countryCode)
+        {
+            // Check if a tab page for the country code already exists
+            foreach (TabPage tabPage in tabControlContacts.TabPages)
+            {
+                if (tabPage.Text == countryCode)
+                {
+                    return tabPage;
+                }
+            }
+
+            // Create a new tab page for the country code
+            TabPage newTabPage = new TabPage(countryCode);
+            tabControlContacts.TabPages.Add(newTabPage);
+
+            // Create a ListBox control for the tab page
+            ListBox listBox = new ListBox();
+            listBox.Dock = DockStyle.Fill;
+            newTabPage.Controls.Add(listBox);
+
+            return newTabPage;
+        }
+
+        private ListBox GetListBoxForTabPage(TabPage tabPage)
+        {
+            // Iterate through the controls of the tab page to find the ListBox
+            foreach (Control control in tabPage.Controls)
+            {
+                if (control is ListBox listBox)
+                {
+                    return listBox;
+                }
+            }
+
+            return null; // ListBox not found
+        }
+
+        private void ExportContactsToCSV()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV Files|*.csv";
+            saveFileDialog.FileName = $"Contacts_by_ContactsManager";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    // Write the headers to the CSV file
+                    writer.WriteLine("Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,Phone 1 - Type,Phone 1 - Value");
+
+                    int contactIndex;
+                    foreach (TabPage tabPage in tabControlContacts.TabPages)
+                    {
+                        string tabName = tabPage.Text;
+                        ListBox listBox = GetListBoxForTabPage(tabPage);
+
+                        contactIndex = 1; // Reset the contact index for each tab
+                        foreach (string contact in listBox.Items)
+                        {
+                            // Generate the contact name using tabName and contactIndex
+                            string contactName = $"{tabName}{contactIndex}";
+
+                            // Generate a random 5-character string
+                            string uniqueString = GenerateRandomString(5);
+
+                            /*// Append the unique string to the contact name
+                            contactName += " " + uniqueString;*/
+
+                            // Write the CSV row with the required fields and values
+                            writer.WriteLine($"\"{contactName}\",,,,,,,,,,,,,,,,,,,,,,,,,,,,\"Contacts Manager ::: * myContacts\",,\"{contact}\"");
+
+                            contactIndex++;
+                        }
+                    }
+                }
+
+
+                MessageBox.Show("Contacts exported to CSV successfully.");
+            }
+        }
+
+        private string GenerateRandomString(int length)
+        {
+            const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            return new string(Enumerable.Repeat(characters, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void btnSaveAll_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt";
+            saveFileDialog.FileName = $"Contacts_by_ContactsManager";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (TabPage tabPage in tabControlContacts.TabPages)
+                    {
+                        writer.WriteLine("country code: " + tabPage.Text);
+
+                        ListBox listBox = GetListBoxForTabPage(tabPage);
+
+                        foreach (string contact in listBox.Items)
+                        {
+                            writer.WriteLine(contact);
+                        }
+
+                        writer.WriteLine();
+                    }
+                }
+
+                MessageBox.Show("Contacts saved successfully.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Add a context menu to the tab control
+            tabControlContacts.ContextMenuStrip = new ContextMenuStrip();
+            tabControlContacts.ContextMenuStrip.Items.Add("Copy Line", null, CopySelectedItem);
+            tabControlContacts.ContextMenuStrip.Items.Add("Copy Tab", null, CopySelectedTab);
+        }
+
+        private void CopySelectedTab(object sender, EventArgs e)
+        {
+            if (tabControlContacts.SelectedTab != null)
+            {
+                TabPage selectedTab = tabControlContacts.SelectedTab;
+
+                StringBuilder sb = new StringBuilder();
+                foreach (Control control in selectedTab.Controls)
+                {
+                    if (control is ListBox listBox)
+                    {
+                        foreach (object item in listBox.Items)
+                        {
+                            sb.AppendLine(item.ToString());
+                        }
+                    }
+                }
+
+                Clipboard.SetText(sb.ToString());
+            }
+        }
+
+        private void CopySelectedItem(object sender, EventArgs e)
+        {
+            TabPage selectedTab = tabControlContacts.SelectedTab;
+            if (selectedTab != null)
+            {
+                ListBox listBox = GetListBoxForTabPage(selectedTab);
+                if (listBox.SelectedItem != null)
+                {
+                    string selectedLine = listBox.SelectedItem.ToString();
+                    Clipboard.SetText(selectedLine);
+                }
+            }
+        }
+
+        private void btnCSV_Click(object sender, EventArgs e)
+        {
+            ExportContactsToCSV();
+        }
+
+        private void btnSaveAllExcel_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xlsx";
+            saveFileDialog.FileName = $"Contacts_by_ContactsManager";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (ExcelPackage package = new ExcelPackage())
+                {
+                    foreach (TabPage tabPage in tabControlContacts.TabPages)
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(tabPage.Text);
+
+                        ListBox listBox = GetListBoxForTabPage(tabPage);
+                        int rowIndex = 1;
+
+                        foreach (string contact in listBox.Items)
+                        {
+                            worksheet.Cells[rowIndex, 1].Value = contact;
+                            rowIndex++;
+                        }
+                    }
+
+                    package.SaveAs(new FileInfo(filePath));
+                }
+
+                MessageBox.Show("Contacts saved to Excel successfully.");
+            }
+        }
+    }
+}
